a7654e897d8a3972646fa178ec2934ec2f8867fa
Adithyan-Raman <36584283+Adithyan-Raman@users.noreply.github.com>
2018-02-28 20:11:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/newfile',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'newfile.html'));
 });
 app.get('article2', function (req, res) {
-    res.send('Article 2 will be sent soon');
+    res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 app.get('article3', function (req, res) {
-    res.send('Article 3 will be sent soon');
+    res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
